c952eaf7599e402d09729432e03d046cfe9df36b
Huu Nguyen <hduyon4@gmail.com>
2026-04-16 15:21:45 +1000

    Changed all file to tsx

---
+++ src/components/ActionShell/FloatingActionButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { colors } from '../../theme';
+import { styles } from '../../styles/FloatingActionButtons.styles';
+
+interface StandardFABProps {
+	onPress?: () => void;
+}
+
+export default function FloatingActionButton({ onPress }: StandardFABProps) {
+	return (
+		<TouchableOpacity activeOpacity={0.8} onPress={onPress}>
+			<LinearGradient
+				colors={[colors.primary, colors.primaryContainer]}
+				start={{ x: 0, y: 0 }}
+				end={{ x: 1, y: 1 }}
+				style={styles.fabStandard}
+			>
+				<MaterialIcons name="add" size={24} color={colors.onPrimary} />
+			</LinearGradient>
+		</TouchableOpacity>
+	);
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#00677f',
+  primaryContainer: '#00d1ff',
+  surface: '#f8f9fa',
+  onSurface: '#191c1d',
+  error: '#ba1a1a',
+};
